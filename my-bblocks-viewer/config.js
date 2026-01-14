43ed958ab1d98888aef83ef2e6166b3935f05f88
avillar <avillar@users.noreply.github.com>
2026-01-14 10:34:20 +0000

    Update directories from master

---
+++ my-bblocks-viewer/config.js
@@ -0,0 +1,6 @@
+window.bblocksRegister = 'https://avillar.github.io/bblocks-sample-gh-pages/build/register.json';
+window.bblocksViewer = {
+  title: "My OGC Building Blocks repository",
+  showImported: 0,
+  baseUrl: '/bblocks-sample-gh-pages/my-bblocks-viewer/',
+};
